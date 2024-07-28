0b7676e8d186dc5b137b54fa9ae0238fd17994ae
A.Kazarinova <fufka93@gmail.com>
2024-07-28 14:35:38 +0300

    create houses-list.create http-request, routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './home/housing-location/housing-location.component';
+import {HousingService} from "./services/housing.service";
+import { DetailsComponent } from './details/details.component';
+import {RouterModule} from '@angular/router';
+import { ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HousingLocationComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [HousingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
